4f33f104de4ba580241c65e8aa6e112ada88289c
Akhil <akhilkum249@gmail.com>
2024-03-23 18:06:59 +0530

    block-BNaacs

---
+++ block-BNaacs/server/school.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+// Setting up template engine
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+// Serving static files from the 'views' directory
+// app.use(express.static("views"));
+
+// Making request
+app.get("/", (req, res) => {
+    res.render("index.ejs", { schoolName: "DAV" });
+});
+
+app.listen(3000, () => {
+    console.log("Listening at port 3000");
+});
